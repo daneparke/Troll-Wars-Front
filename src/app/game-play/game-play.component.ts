0139239460cae7052f3dc9552fd2cb01b88605ba
daneparke <dapa8522@colorado.edu>
2018-12-19 08:40:20 -0700

    added all squad units

--- src/app/game-play/game-play.component.ts
+++ src/app/game-play/game-play.component.ts
@@ -10,9 +10,9 @@ import { TrolltollService } from './trolltoll.service';
 })
 export class GamePlayComponent implements OnInit {
   public units = [];
-  start:boolean = false;
+  start: boolean = false;
   constructor(private _TrolltollService: TrolltollService) { }
-  
+
   ngOnInit() {
     this._TrolltollService.getUnits()
       .subscribe(data => {
@@ -20,16 +20,30 @@ export class GamePlayComponent implements OnInit {
         this.start = true;
 
       })
-      this._TrolltollService.getBoard()
-      this.setBoard()
+    this._TrolltollService.getBoard()
+    this.setBoard()
   }
-  setBoard(){
-    
-    let knight = this.units.filter(unit => unit.id===1)[0]
+  setBoard() {
+
+    let knight = this.units.filter(unit => unit.id === 1)[0]
+    let mage = this.units.filter(unit => unit.id === 2)[0]
+    let ranger = this.units.filter(unit => unit.id === 3)[0]
+    let cleric = this.units.filter(unit => unit.id === 4)[0]
+
+
     this._TrolltollService.board.map(position => {
       console.log("hi dane")
-      if((position.id===143) || (position.id===2)) {
-         position.piece = knight
+      if ((position.id === 129) || (position.id === 130) || (position.id === 131) || (position.id === 14) || (position.id === 15) || (position.id === 16)) {
+        position.piece = knight
+      }
+      else if ((position.id === 141) || (position.id === 144) || (position.id === 1) || (position.id === 4)) {
+        position.piece = ranger
+      }
+      else if ((position.id === 143) || (position.id === 2)) {
+        position.piece = cleric
+      }
+      else if ((position.id === 142) || (position.id === 3)) {
+        position.piece = mage
       }
       return position
     })
