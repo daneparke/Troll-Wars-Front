8cf1247b0cc93e104ea35bc6451c825eb5165424
zipit145 <gregbforney@gmail.com>
2019-01-02 14:54:33 -0700

    footer stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GamePlayComponent } from './game-play/game-play.component';
 import { HttpClientModule } from '@angular/common/http'
@@ -14,7 +13,6 @@ import { TrolltollService } from './game-play/trolltoll.service';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     LoginPageComponent,
     GamePlayComponent
   ],
